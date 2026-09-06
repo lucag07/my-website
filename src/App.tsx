bc61577244e4bbd205b8a5e70f973f593057b4e5
lucag07 <luca@gettasklumas.com>
2026-09-06 13:28:01 +0100

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { Landing } from "./pages/Landing";
-import { AreasWeServe } from "./pages/AreasWeServe";
-import { RegionPage } from "./pages/RegionPage";
-import { CityPage } from "./pages/CityPage";
 
 function App() {
   return (
@@ -11,9 +8,6 @@ function App() {
       <Toaster position="top-right" />
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/locations" element={<AreasWeServe />} />
-        <Route path="/locations/:region" element={<RegionPage />} />
-        <Route path="/locations/:region/:city" element={<CityPage />} />
       </Routes>
     </Router>
   );
