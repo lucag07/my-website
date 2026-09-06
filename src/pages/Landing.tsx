23936cb74dc1de0e9e39960382438a07f9d8c055
lucag07 <luca@gettasklumas.com>
2026-09-06 13:24:30 +0100

    undo change

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -7,7 +7,6 @@ import { TrustSignalsStrip } from "../components/TrustSignalsStrip";
 import { MetricsSection } from "../components/MetricsSection";
 import { FAQSection } from "../components/FAQSection";
 import { ProcessSequence } from "../components/ProcessSequence";
-import { LocationsSection } from "../components/LocationsSection";
 import { FinalCTAStrip } from "../components/FinalCTAStrip";
 import { StickyHeader } from "../components/StickyHeader";
 import { ExitIntentModal } from "../components/ExitIntentModal";
@@ -27,7 +26,6 @@ export function Landing() {
       <MetricsSection />
       <FAQSection />
       <ProcessSequence />
-      <LocationsSection />
       <FinalCTAStrip />
       <Footer />
     </div>
